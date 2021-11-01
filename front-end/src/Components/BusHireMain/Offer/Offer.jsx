91a0214213b6661048f2ad2734ae848f6f1775ab
RanuRanjan <ranuranjan25@gmail.com>
2021-11-01 16:59:04 +0530

    bus hire page  offer components

---
+++ front-end/src/Components/BusHireMain/Offer/Offer.jsx
@@ -0,0 +1,15 @@
+import './Offer.css'
+export const Offer=()=>{
+    return(
+        <div className="Offer">
+             <div className="OffersHeading">
+                 Offers
+             </div>
+             <div className="OfferButton">
+                 <img className="offergiff" src="https://www.redbus.in/bushire/static/icons/OfferImage.svg" alt="" />
+                 <div className="offergetupto">Get upto Rs.1000 Off</div>
+                 <img className="arrowoffer" src="https://www.redbus.in/bushire/static/icons/curvedRightArrow.svg" alt="" />
+             </div>
+         </div>
+    )
+}
