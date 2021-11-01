0e83fe9cf2f081c1d645ab9661f1eba33733034d
RanuRanjan <ranuranjan25@gmail.com>
2021-11-01 14:56:54 +0530

    bus hire page banner with background image completed

--- front-end/src/Components/BusHireMain/Banner.jsx/BushireBanner.jsx
+++ front-end/src/Components/BusHireMain/Banner.jsx/BushireBanner.jsx
@@ -1,15 +1,27 @@
-import './Banner.css'
+import "./Banner.css";
 
-export const Banner= ()=>{
-    return(
-        <div className="MainBanner">
-            <div className="BushireBanner">
-             </div>
-            <div className="BannerInner">
-                <img src="https://www.redbus.in/bushire/static/webv2/home/group-4.svg" alt="" />
-                <div>
-                </div>
+export const Banner = () => {
+  return (
+    <div className="MainBanner">
+      <div className="BushireBanner"></div>
+      <div className="BannerInner">
+        {/* <img
+          src="https://www.redbus.in/bushire/static/webv2/home/group-4.svg"
+          alt="bannerImage"
+        /> */}
+        <div className="BannerElements">
+            <div className="BannerHeading">Bus Hire</div>
+            <div className="BannerSubHeading">
+                Rent vehicle with a driver from
+                <br />
+                the best operators
             </div>
+            <div className="SvgElements">
+                  <img src="https://www.redbus.in/bushire/static/webv2/home/3%20(1).svg"alt="buslogo"/> 
+            </div>
+            
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
