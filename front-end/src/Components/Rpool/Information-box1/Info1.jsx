9c87c4fb629d834b6bf418ff283fe307e16b43c4
RaniKamna <kamna.a139@gmail.com>
2021-11-02 23:44:37 +0530

    info1

--- front-end/src/Components/Rpool/Information-box1/Info1.jsx
+++ front-end/src/Components/Rpool/Information-box1/Info1.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
 import styles from './Info1.module.css'
 export const Info1 = () => {
+    const [style, setStyle] = useState(styles.offer)
+    const [change, setChange] = useState(styles.find)
+    const [num1, setNum1] = useState(styles.number)
+    const [num2, setNum2] = useState(styles.number)
+    const [num3, setNum3] = useState(styles.number)
+    const [num4, setNum4] = useState(styles.number)
+
+    const numclr1 = () => {
+        setNum1(styles.numbersel)
+    }
+    const numclr2 = () => {
+        setNum2(styles.numbersel)
+    }
+    const numclr3 = () => {
+        setNum3(styles.numbersel)
+    }
+    const numclr4 = () => {
+        setNum4(styles.numbersel)
+    }
+
+    const changeclr = () => {
+        setStyle(styles.find)
+        //setChange(styles.offer)
+    }
+
     return (
         <>
             <div>
@@ -8,30 +34,30 @@ export const Info1 = () => {
                         <h2 className={styles.heading}>How it works</h2>
                         <div className={styles.toglbtn}>
                             <span className={styles.find}>Find Ride</span>
-                            <span className={styles.offer}>Offer Ride</span>
+                            <span onClick={changeclr} className={style}>Offer Ride</span>
                         </div>
                         <div>
                             <div className={styles.tabsel}>
-                                <div className={styles.activate}>
-                                    <div className={styles.number}>1</div>
+                                <div onClick={numclr1} className={styles.activate}>
+                                    <div className={num1}>1</div>
                                     <div className={styles.text}>
                                         <span className={styles.txt}><b>Activate</b> account.</span>
                                     </div>
                                 </div>
-                                <div className={styles.activate}>
-                                    <div className={styles.number}>2</div>
+                                <div onClick={numclr2} className={styles.activate}>
+                                    <div className={num2}>2</div>
                                     <div className={styles.text}>
                                         <span className={styles.txt}><b>Request</b> a ride .</span>
                                     </div>
                                 </div>
-                                <div className={styles.activate}>
-                                    <div className={styles.number}>3</div>
+                                <div onClick={numclr3} className={styles.activate}>
+                                    <div className={num3}>3</div>
                                     <div className={styles.text}>
                                         <span className={styles.txt}><b>Choose</b> from ride.</span>
                                     </div>
                                 </div>
-                                <div className={styles.activate}>
-                                    <div className={styles.number}>4</div>
+                                <div onClick={numclr4} className={styles.activate}>
+                                    <div className={num4}>4</div>
                                     <div className={styles.text}>
                                         <span className={styles.txt}><b>Pay</b>using points.</span>
                                     </div>
