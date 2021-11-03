b20ec513a18ac17f4d87fc3b39e244a098069e44
RaniKamna <kamna.a139@gmail.com>
2021-11-03 21:32:06 +0530

    link added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,7 @@
 import './App.css';
 import { Route, Switch, Link } from 'react-router-dom'
 import { LandingPage } from "./Components/LandingPage/LandingPage.jsx";
-import { Nav1 } from './Components/Rpool/Navbar1/Navbar1';
-import { Nav2 } from './Components/Rpool/Navbar2/Navbar2';
-import { Bannerbox } from './Components/Rpool/Bannerbox/Banner';
-import { Info1 } from './Components/Rpool/Information-box1/Info1';
-import { Info2 } from './Components/Rpool/Information-box2/Info2';
-import { Info3 } from './Components/Rpool/Information-box3/Info3';
-import { Info4 } from './Components/Rpool/Information-box4/Info4';
-import { Info5 } from './Components/Rpool/Information-box5/Info5';
-import { FooterR } from './Components/Rpool/Footer-rpool/Footer';
+import { Rpool } from './Components/Rpool/Rpool';
 import { Bushire } from './Components/BusHireMain/BusHire'
 
 function App() {
@@ -23,15 +15,7 @@ function App() {
           <LandingPage />
         </Route>
         <Route path='/rpool' exact>
-          <Nav1 />
-          <Bannerbox />
-          <Nav2 />
-          <Info1 />
-          <Info2 />
-          <Info3 />
-          <Info4 />
-          <Info5 />
-          <FooterR />
+          <Rpool />
         </Route>
         <Route path='/bushire' exact>
           <Bushire />
