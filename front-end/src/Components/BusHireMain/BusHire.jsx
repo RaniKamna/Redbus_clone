8fd5a8a6471ddc6c26300f0426d533243ee8d222
RanuRanjan <ranuranjan25@gmail.com>
2021-11-09 21:06:00 +0530

    docs added

--- front-end/src/Components/BusHireMain/BusHire.jsx
+++ front-end/src/Components/BusHireMain/BusHire.jsx
@@ -6,6 +6,8 @@ import { VechileType } from "./Vechicle type/VechicleType"
 import { WhyBook } from "./WhyBook/WhyBook"
 import { WithBusHire } from "./WithBusHire/WithBusHire"
 import { Operates } from "./BusOperates/BusOperates"
+import FaqSection from "./FAQ/FAQ"
+import {Docs} from './HireDocs/Docs'
 
 export const Bushire=()=>{
     return(
@@ -18,6 +20,8 @@ export const Bushire=()=>{
             <VechileType/>
             <WhyBook/>
             <Operates/>
+            <FaqSection/>
+            <Docs/>
         </div>
     )
 }
