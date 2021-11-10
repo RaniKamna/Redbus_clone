f22be6e2ab6c00d9cb5391c074ef192481465711
RanuRanjan <ranuranjan25@gmail.com>
2021-11-10 11:59:11 +0530

    bus hire

--- front-end/src/Components/BusHireMain/BusHire.jsx
+++ front-end/src/Components/BusHireMain/BusHire.jsx
@@ -8,6 +8,8 @@ import { WithBusHire } from "./WithBusHire/WithBusHire"
 import { Operates } from "./BusOperates/BusOperates"
 import FaqSection from "./FAQ/FAQ"
 import {Docs} from './HireDocs/Docs'
+import {SubFooter} from '../Footer/SubFooter'
+import { Footer } from "../Footer/Footer"
 
 export const Bushire=()=>{
     return(
@@ -22,6 +24,8 @@ export const Bushire=()=>{
             <Operates/>
             <FaqSection/>
             <Docs/>
+            <SubFooter/>
+            <Footer/>
         </div>
     )
 }
