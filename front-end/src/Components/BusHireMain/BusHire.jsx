46c0fb787341e136d10f51edbd5ffa5ee47dddd7
RanuRanjan <ranuranjan25@gmail.com>
2021-11-10 12:03:30 +0530

    footer completed

--- front-end/src/Components/BusHireMain/BusHire.jsx
+++ front-end/src/Components/BusHireMain/BusHire.jsx
@@ -6,14 +6,10 @@ import { VechileType } from "./Vechicle type/VechicleType"
 import { WhyBook } from "./WhyBook/WhyBook"
 import { WithBusHire } from "./WithBusHire/WithBusHire"
 import { Operates } from "./BusOperates/BusOperates"
-<<<<<<< HEAD
 import FaqSection from "./FAQ/FAQ"
 import {Docs} from './HireDocs/Docs'
 import {SubFooter} from '../Footer/SubFooter'
 import { Footer } from "../Footer/Footer"
-=======
-
->>>>>>> f2763c1da5d93f812019c3ff6e96662368af9bc7
 
 export const Bushire=()=>{
     return(
@@ -26,13 +22,10 @@ export const Bushire=()=>{
             <VechileType/>
             <WhyBook/>
             <Operates/>
-<<<<<<< HEAD
             <FaqSection/>
             <Docs/>
             <SubFooter/>
             <Footer/>
-=======
->>>>>>> f2763c1da5d93f812019c3ff6e96662368af9bc7
         </div>
     )
 }
