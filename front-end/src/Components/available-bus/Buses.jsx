e65d306ddfb61c1db14cfaae759ca5f3fa838a87
Mansur Dewan <mansurdewan26@gmail.com>
2021-11-12 10:17:10 +0530

    Static bus data

--- front-end/src/Components/available-bus/Buses.jsx
+++ front-end/src/Components/available-bus/Buses.jsx
@@ -5,21 +5,192 @@ import "../LandingPage/searchBuses.css"
 
 
 function Buses() {
-    const [buses, setBuses] = useState();
+
+
+
+
+    const All_bus = [{
+        "_id": "618b660ca17ac455f40e10ef",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 1)",
+        "pick_up_time": "2",
+        "duration": "3.30",
+        "drop_time": "5.30",
+        "rating": 1,
+        "price": 425,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "durgapur",
+        "drop_address": "howrah",
+        "passengers": [],
+        "travel_name": "JGD Travels Pvt Ltd.",
+        "__v": 0
+    },
+    {
+        "_id": "618b6662a17ac455f40e10f1",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "2.30",
+        "duration": "3.30",
+        "drop_time": "6.00",
+        "rating": 1.9,
+        "price": 525,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "durgapur",
+        "drop_address": "howrah",
+        "passengers": [],
+        "travel_name": "JGD Travels Pvt Ltd.",
+        "__v": 0
+    },
+    {
+        "_id": "618b67c223e97c3fcee6f2b6",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "2.30",
+        "duration": "3.30",
+        "drop_time": "6.00",
+        "rating": 1.9,
+        "price": 525,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "durgapur",
+        "drop_address": "howrah",
+        "passengers": [],
+        "sleeper": true,
+        "ac": false,
+        "nonac": false,
+        "travel_name": "Hans Travels (I) Pvt Ltd.",
+        "__v": 0
+    },
+    {
+        "_id": "618b683d23e97c3fcee6f2b8",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "5.30",
+        "duration": "40.00",
+        "drop_time": "9.25",
+        "rating": 2.2,
+        "price": 2600,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "mumbai",
+        "drop_address": "new delhi",
+        "passengers": [],
+        "sleeper": false,
+        "ac": true,
+        "nonac": false,
+        "travel_name": "Hans Travels (I) Pvt Ltd.",
+        "__v": 0
+    },
+    {
+        "_id": "618b691d23e97c3fcee6f2ba",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "16.30",
+        "duration": "44.00",
+        "drop_time": "12.25",
+        "rating": 2.7,
+        "price": 3000,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "mumbai",
+        "drop_address": "new delhi",
+        "passengers": [],
+        "sleeper": false,
+        "ac": true,
+        "nonac": false,
+        "travel_name": "Hans Travels (I) Pvt Ltd.",
+        "__v": 0
+    },
+    {
+        "_id": "618b69e223e97c3fcee6f2bc",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "17.00",
+        "duration": "21.00",
+        "drop_time": "13.30",
+        "rating": 3.9,
+        "price": 1524,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "bangalore",
+        "drop_address": "mumbai",
+        "passengers": [],
+        "sleeper": false,
+        "ac": false,
+        "nonac": true,
+        "travel_name": "Primo National travels.",
+        "__v": 0
+    },
+    {
+        "_id": "618b6a2423e97c3fcee6f2be",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "14.10",
+        "duration": "21.00",
+        "drop_time": "11.30",
+        "rating": 3.9,
+        "price": 1524,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "bangalore",
+        "drop_address": "mumbai",
+        "passengers": [],
+        "sleeper": true,
+        "ac": false,
+        "nonac": false,
+        "travel_name": "Fexi Ticket National travels.",
+        "__v": 0
+    },
+    {
+        "_id": "618bca61b4d9551885665441",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": "14.10",
+        "duration": "21.00",
+        "drop_time": "11.30",
+        "rating": 3.9,
+        "price": 1524,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "bangalore",
+        "drop_address": "mumbai",
+        "passengers": [],
+        "sleeper": true,
+        "ac": false,
+        "nonac": false,
+        "travel_name": "Fexi Ticket National travels.",
+        "__v": 0
+    },
+    {
+        "_id": "618d192d3ddf348b056907cf",
+        "bus_name": "Bharat Benz A/C Sleeper(2 + 2)",
+        "pick_up_time": 14.1,
+        "duration": 21,
+        "drop_time": 11.3,
+        "rating": 3.9,
+        "price": 1524,
+        "seats_available": 34,
+        "seat_type": "single",
+        "pickup_address": "bangalore",
+        "drop_address": "mumbai",
+        "passengers": [],
+        "sleeper": true,
+        "ac": false,
+        "nonac": false,
+        "travel_name": "Fexi Ticket National travels.",
+        "__v": 0
+    }]
+
+    const [buses, setBuses] = useState(All_bus);
     
     //  const [loading, setLoading] = useState(true);
-    const getBuses = async() => {
-        let res = await fetch("http://localhost:2233/booking/data");
-        let busesa = await res.json();
-        setBuses(busesa.item)
+    // const getBuses = () => {
+    //     // let res = await fetch("http://localhost:2233/booking/data");
+    //     // let busesa = await res.json();
+    //     setBuses(All_bus)
 
-    }
+    // }
    
 
-    useEffect(() => {
-        getBuses();
-        // setLoading(false);
-    }, []);
+    // useEffect(() => {
+    //     getBuses();
+    //     console.log(buses);
+    //     // setLoading(false);
+    // }, []);
     // console.log(buses);
     // if(buses.length !== 0) {
     //     setLoading(false)
